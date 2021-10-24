322d502770e7b27bf4206077fb67f3de5cef16ff
andrewsoon <andrewsoon.tx@gmail.com>
2021-10-24 23:38:46 +0800

    Navbar updates

--- javascript/index.js
+++ javascript/index.js
@@ -30,10 +30,24 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
-  if (!event.target.matches("#header-link2")) {
+  if (!event.target.matches("#header-link")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+  if (!event.target.matches("#header-link2")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
