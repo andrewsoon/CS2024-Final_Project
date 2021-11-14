b7e498c9e7f38185b5cc045229d54a229559587d
andrewsoon <andrewsoon.tx@gmail.com>
2021-11-14 20:48:49 +0800

    Update form

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -1,8 +1,19 @@
 function showDay(element) {
   document.getElementById("classes").style.display =
     element.value == 0 ? "block" : "none";
-  // document.getElementById("above12-dates").style.display =
-  //   element.value == 1 ? "block" : "none";
-  // document.getElementById("holiday-dates").style.display =
-  //   element.value == 2 ? "block" : "none";
+  document.getElementById("holiday1").style.display =
+    element.value == 1 ? "block" : "none";
+  document.getElementById("holiday2").style.display =
+    element.value == 2 ? "block" : "none";
+  document.getElementById("holiday3").style.display =
+    element.value == 3 ? "block" : "none";
+}
+
+function showTimeslot(element) {
+  document.getElementById("weekdays").style.display =
+    element.value == 0 ? "block" : "none";
+  document.getElementById("weekends").style.display =
+    element.value == 1 ? "block" : "none";
+  document.getElementById("freetrial").style.display =
+    element.value == 2 ? "block" : "none";
 }
